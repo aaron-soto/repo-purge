f4c89cac8619c80dac40f44fbf6d83bc418723ae
aaron-soto <aaron.m.soto1@gmail.com>
2024-06-03 11:31:14 -0700

    chore: Update .gitignore to include .env and .vercel files

--- src/app/repos/columns.tsx
+++ src/app/repos/columns.tsx
@@ -1,10 +1,10 @@
-"use client";
+'use client';
 
-import { useState } from "react";
-import { ArrowUpDown, MoreHorizontal } from "lucide-react";
-import { Repo } from "@/types/types";
-import { ColumnDef } from "@tanstack/react-table";
-import { Button } from "@/components/ui/button";
+import { useState } from 'react';
+import { ArrowUpDown, MoreHorizontal } from 'lucide-react';
+import { Repo } from '@/types/types';
+import { ColumnDef } from '@tanstack/react-table';
+import { Button } from '@/components/ui/button';
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -12,34 +12,121 @@ import {
   DropdownMenuLabel,
   DropdownMenuSeparator,
   DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-import { Checkbox } from "@/components/ui/checkbox";
-import { useToast } from "@/components/ui/use-toast";
+} from '@/components/ui/dropdown-menu';
+import { Checkbox } from '@/components/ui/checkbox';
+import { useToast } from '@/components/ui/use-toast';
 import {
   Dialog,
   DialogContent,
   DialogDescription,
   DialogHeader,
   DialogTitle,
-} from "@/components/ui/dialog";
+} from '@/components/ui/dialog';
+
+const RepoActionsCell = ({ row }: any) => {
+  const repo: Repo = row.original;
+  const { toast } = useToast();
+  const [dialogOpen, setDialogOpen] = useState(false);
+  const [selectedRepo, setSelectedRepo] = useState<Repo | null>(null);
+
+  const handleDelete = () => {
+    setDialogOpen(false);
+    if (selectedRepo) {
+      // Delete the repo
+
+      toast({
+        title: 'Repository deleted',
+        variant: 'destructive',
+        description: `${selectedRepo.name} has been deleted!`,
+      });
+      setSelectedRepo(null);
+    }
+  };
+
+  const handleOpenDialog = (repo: Repo) => {
+    setSelectedRepo(repo);
+    setDialogOpen(true);
+  };
+
+  return (
+    <>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="ghost" className="w-8 h-8 p-0">
+            <span className="sr-only">Open menu</span>
+            <MoreHorizontal className="w-4 h-4" />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end">
+          <DropdownMenuLabel>Actions</DropdownMenuLabel>
+          <DropdownMenuItem
+            onClick={() => {
+              navigator.clipboard.writeText(repo.html_url);
+              toast({
+                title: 'Copied to clipboard',
+                description: 'The Repo URL has been copied!',
+              });
+            }}
+          >
+            Copy URL
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            <a href={repo.html_url} target="_blank">
+              Visit Repo
+            </a>
+          </DropdownMenuItem>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem
+            className="text-red-500"
+            onClick={() => handleOpenDialog(repo)}
+          >
+            Delete Repo
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+
+      <Dialog open={dialogOpen} onOpenChange={setDialogOpen}>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>
+              Delete &apos;{selectedRepo?.name}&apos; repository?
+            </DialogTitle>
+            <DialogDescription>
+              This action cannot be undone. This will delete the repository from
+              GitHub.
+            </DialogDescription>
+            <div className="flex justify-end gap-4 mt-4">
+              <Button variant="outline" onClick={() => setDialogOpen(false)}>
+                Cancel
+              </Button>
+              <Button variant="destructive" onClick={handleDelete}>
+                Delete
+              </Button>
+            </div>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
+    </>
+  );
+};
 
 export const columns: ColumnDef<Repo>[] = [
   {
-    id: "select",
+    id: 'select',
     header: ({ table }) => (
       <Checkbox
         checked={
           table.getIsAllPageRowsSelected() ||
-          (table.getIsSomePageRowsSelected() && "indeterminate")
+          (table.getIsSomePageRowsSelected() && 'indeterminate')
         }
-        onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+        onCheckedChange={value => table.toggleAllPageRowsSelected(!!value)}
         aria-label="Select all"
       />
     ),
     cell: ({ row }) => (
       <Checkbox
         checked={row.getIsSelected()}
-        onCheckedChange={(value) => row.toggleSelected(!!value)}
+        onCheckedChange={value => row.toggleSelected(!!value)}
         aria-label="Select row"
       />
     ),
@@ -47,121 +134,33 @@ export const columns: ColumnDef<Repo>[] = [
     enableHiding: false,
   },
   {
-    accessorKey: "name",
+    accessorKey: 'name',
     header: ({ column }) => {
       return (
         <Button
           variant="ghost"
-          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
         >
           Name
-          <ArrowUpDown className="ml-2 h-4 w-4" />
+          <ArrowUpDown className="w-4 h-4 ml-2" />
         </Button>
       );
     },
   },
   {
-    accessorKey: "description",
-    header: "Description",
+    accessorKey: 'description',
+    header: 'Description',
     cell: ({ getValue }) => {
       const description = getValue();
       return description ? description : <span className="text-muted">-</span>;
     },
   },
   {
-    accessorKey: "visibility",
-    header: "Visibility",
+    accessorKey: 'visibility',
+    header: 'Visibility',
   },
   {
-    id: "actions",
-    cell: ({ row }) => {
-      const repo: Repo = row.original;
-      const { toast } = useToast();
-      const [dialogOpen, setDialogOpen] = useState(false);
-      const [selectedRepo, setSelectedRepo] = useState<Repo | null>(null);
-
-      const handleDelete = () => {
-        setDialogOpen(false);
-        if (selectedRepo) {
-          // Delete the repo
-
-          toast({
-            title: "Repository deleted",
-            variant: "destructive",
-            description: `${selectedRepo.name} has been deleted!`,
-          });
-          setSelectedRepo(null);
-        }
-      };
-
-      const handleOpenDialog = (repo: Repo) => {
-        setSelectedRepo(repo);
-        setDialogOpen(true);
-      };
-
-      return (
-        <>
-          <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-              <Button variant="ghost" className="h-8 w-8 p-0">
-                <span className="sr-only">Open menu</span>
-                <MoreHorizontal className="h-4 w-4" />
-              </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent align="end">
-              <DropdownMenuLabel>Actions</DropdownMenuLabel>
-              <DropdownMenuItem
-                onClick={() => {
-                  navigator.clipboard.writeText(repo.html_url);
-                  toast({
-                    title: "Copied to clipboard",
-                    description: "The Repo URL has been copied!",
-                  });
-                }}
-              >
-                Copy URL
-              </DropdownMenuItem>
-              <DropdownMenuItem>
-                <a href={repo.html_url} target="_blank">
-                  Visit Repo
-                </a>
-              </DropdownMenuItem>
-              <DropdownMenuSeparator />
-              <DropdownMenuItem
-                className="text-red-500"
-                onClick={() => handleOpenDialog(repo)}
-              >
-                Delete Repo
-              </DropdownMenuItem>
-            </DropdownMenuContent>
-          </DropdownMenu>
-
-          <Dialog open={dialogOpen} onOpenChange={setDialogOpen}>
-            <DialogContent>
-              <DialogHeader>
-                <DialogTitle>
-                  Delete '{selectedRepo?.name}' repository?
-                </DialogTitle>
-                <DialogDescription>
-                  This action cannot be undone. This will delete the repository
-                  from github.
-                </DialogDescription>
-                <div className="flex justify-end gap-4 mt-4">
-                  <Button
-                    variant="outline"
-                    onClick={() => setDialogOpen(false)}
-                  >
-                    Cancel
-                  </Button>
-                  <Button variant="destructive" onClick={handleDelete}>
-                    Delete
-                  </Button>
-                </div>
-              </DialogHeader>
-            </DialogContent>
-          </Dialog>
-        </>
-      );
-    },
+    id: 'actions',
+    cell: RepoActionsCell,
   },
 ];
