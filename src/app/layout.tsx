33bff6571bed431ec4f18ddb839159bf78654ecc
aaron-soto <aaron.m.soto1@gmail.com>
2024-06-04 07:57:04 -0700

    changed to vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ import { cn } from '@/lib/utils';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
-import Analytics from '@/components/Analytics';
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -23,75 +23,77 @@ export default function RootLayout({
 }) {
 
   return (
-    <SessionWrapper>
-      <TooltipProvider>
-        <html lang="en">
-          <head>
-            <meta
-              name="keywords"
-              content="GitHub, Repository, Delete, Bulk, Tool, Repo Purge"
-            />
-            <meta name="author" content="Aaron Soto" />
-            <meta name="robots" content="index, follow" />
-            <meta
-              name="viewport"
-              content="width=device-width, initial-scale=1"
-            />
-            <link rel="canonical" href="https://www.repopurge.com" />
+    <>
+      <SessionWrapper>
+        <TooltipProvider>
+          <html lang="en">
+            <head>
+              <meta
+                name="keywords"
+                content="GitHub, Repository, Delete, Bulk, Tool, Repo Purge"
+              />
+              <meta name="author" content="Aaron Soto" />
+              <meta name="robots" content="index, follow" />
+              <meta
+                name="viewport"
+                content="width=device-width, initial-scale=1"
+              />
+              <link rel="canonical" href="https://www.repopurge.com" />
 
-            <meta property="og:title" content="Repo Purge" />
-            <meta
-              property="og:description"
-              content="Github Repository bulk delete tool"
-            />
-            <meta property="og:type" content="website" />
-            <meta property="og:url" content="https://www.repopurge.com" />
-            <meta property="og:image" content="/images/og-image.png" />
-            <meta property="og:site_name" content="Repo Purge" />
-            <meta property="og:locale" content="en_US" />
+              <meta property="og:title" content="Repo Purge" />
+              <meta
+                property="og:description"
+                content="Github Repository bulk delete tool"
+              />
+              <meta property="og:type" content="website" />
+              <meta property="og:url" content="https://www.repopurge.com" />
+              <meta property="og:image" content="/images/og-image.png" />
+              <meta property="og:site_name" content="Repo Purge" />
+              <meta property="og:locale" content="en_US" />
 
-            <link
-              rel="apple-touch-icon"
-              sizes="180x180"
-              href="/images/favicons/apple-touch-icon.png"
-            />
-            <link
-              rel="icon"
-              type="image/png"
-              sizes="32x32"
-              href="/images/favicons/favicon-32x32.png"
-            />
-            <link
-              rel="icon"
-              type="image/png"
-              sizes="16x16"
-              href="/images/favicons/favicon-16x16.png"
-            />
-            <link rel="manifest" href="/images/favicons/site.webmanifest" />
-            <link
-              rel="mask-icon"
-              href="/images/favicons/safari-pinned-tab.svg"
-              color="#5b94d5"
-            />
-            <link rel="shortcut icon" href="/images/favicons/favicon.ico" />
-            <meta name="msapplication-TileColor" content="#ffc40d" />
-            <meta
-              name="msapplication-config"
-              content="/images/favicons/browserconfig.xml"
-            />
-            <meta name="theme-color" content="#000000" />
-          </head>
-          <body className={cn(inter.className, 'dark bg-black flex flex-col')}>
-            <Navbar />
-            <main className="pt-18">{children}</main>
-            <Analytics />
-            <Footer />
-            <div className="z-50">
-              <Toaster />
-            </div>
-          </body>
-        </html>
-      </TooltipProvider>
-    </SessionWrapper>
+              <link
+                rel="apple-touch-icon"
+                sizes="180x180"
+                href="/images/favicons/apple-touch-icon.png"
+              />
+              <link
+                rel="icon"
+                type="image/png"
+                sizes="32x32"
+                href="/images/favicons/favicon-32x32.png"
+              />
+              <link
+                rel="icon"
+                type="image/png"
+                sizes="16x16"
+                href="/images/favicons/favicon-16x16.png"
+              />
+              <link rel="manifest" href="/images/favicons/site.webmanifest" />
+              <link
+                rel="mask-icon"
+                href="/images/favicons/safari-pinned-tab.svg"
+                color="#5b94d5"
+              />
+              <link rel="shortcut icon" href="/images/favicons/favicon.ico" />
+              <meta name="msapplication-TileColor" content="#ffc40d" />
+              <meta
+                name="msapplication-config"
+                content="/images/favicons/browserconfig.xml"
+              />
+              <meta name="theme-color" content="#000000" />
+            </head>
+            <body className={cn(inter.className, 'dark bg-black flex flex-col')}>
+              <Navbar />
+              <main className="pt-18">{children}</main>
+              <Footer />
+              <div className="z-50">
+                <Toaster />
+              </div>
+            </body>
+          </html>
+        </TooltipProvider>
+      </SessionWrapper>
+      <Analytics />
+    </>
   );
 }
